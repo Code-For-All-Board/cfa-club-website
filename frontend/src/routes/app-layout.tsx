69630c150b2c651804fd834a89e4f65426721be0
kyoshinoda <kyoshisew@gmail.com>
2026-04-15 12:41:07 -0400

    added navbar

--- frontend/src/routes/app-layout.tsx
+++ frontend/src/routes/app-layout.tsx
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router";
 
-import { SiteNav } from "../components/site-nav";
+import { NavBar } from "../components/NavBar";
 
 export default function AppLayout() {
   return (
-    <div className="min-h-screen bg-slate-100">
-      <SiteNav />
-      <main className="px-6 py-10">
+    <div className="min-h-screen bg-[#f4f4f8]">
+      <NavBar />
+      <main className="px-6 py-8 sm:py-10">
         <Outlet />
       </main>
     </div>
