3fa3abb73138a80dfa427817e8ef96c31b5c6d45
kyoshinoda <kyoshisew@gmail.com>
2026-04-15 13:00:27 -0400

    added footer

--- frontend/src/routes/app-layout.tsx
+++ frontend/src/routes/app-layout.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router";
 
+import { Footer } from "../components/Footer";
 import { NavBar } from "../components/NavBar";
 
 export default function AppLayout() {
   return (
-    <div className="min-h-screen bg-[#f4f4f8]">
+    <div className="flex min-h-screen flex-col bg-[#f4f4f8]">
       <NavBar />
-      <main className="px-6 py-8 sm:py-10">
+      <main className="flex-1 px-6 py-8 sm:py-10">
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 }
